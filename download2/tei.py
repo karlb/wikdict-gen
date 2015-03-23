85b6a887729e19b9e65e7eabee57606056c4dc45
Karl Bartel <karl42@gmail.com>
2015-03-23 15:40:03 +0100

    link ontology

--- download2/tei.py
+++ download2/tei.py
@@ -26,6 +26,24 @@ def indent(elem, level=0):
             elem.tail = i
 
 
+pos_mapping = {
+    'adjective': ('adj', 'FreeDict_ontology.xml#f_pos_adj'),
+    'adverb': ('adv', 'FreeDict_ontology.xml#f_pos_adv'),
+    'noun': ('n', 'FreeDict_ontology.xml#f_pos_noun'),
+    'properNoun': ('pn', 'FreeDict_ontology.xml#f_pos_noun'),
+    'verb': ('v', 'FreeDict_ontology.xml#f_pos_verb'),
+    # other pos from ontology which are not used, yet:
+    # <item ana="FreeDict_ontology.xml#f_pos_v-intrans">vi</item>
+    # <item ana="FreeDict_ontology.xml#f_pos_v-trans">vt</item>
+    # <item ana="FreeDict_ontology.xml#f_pos_num">num</item>
+    # <item ana="FreeDict_ontology.xml#f_pos_prep">prep</item>
+    # <item ana="FreeDict_ontology.xml#f_pos_int">int</item>
+    # <item ana="FreeDict_ontology.xml#f_pos_pron">pron</item>
+    # <item ana="FreeDict_ontology.xml#f_pos_conj">conj</item>
+    # <item ana="FreeDict_ontology.xml#f_pos_art">art</item>
+}
+
+
 Translation = namedtuple('Translation', ['word', 'pos', 'gender', 'sense',
                                          'sense_num', 'trans_list',
                                          'pronun_list'])
@@ -38,6 +56,9 @@ headwords = int(subprocess.check_output("cut -f 1 -d $'\t' %s | uniq | wc -l"
 with codecs.open(in_filename, 'r', 'utf-8') as in_file, \
         open(out_filename, 'w') as out_file:
 
+    pos_usage = ''.join('<item ana="{1}">{0}</item>'.format(*pos)
+                        for pos in pos_mapping.values())
+
     # prepare header
     out_file.write("""
         <?xml version="1.0" encoding="UTF-8"?>
@@ -49,9 +70,13 @@ with codecs.open(in_filename, 'r', 'utf-8') as in_file, \
         <teiHeader xml:lang="en">
             <fileDesc>
                 <titleStmt>
-                    <title>WikDict.com {from_name}-{to_name} dictionary</title>
+                    <title>{from_name}-{to_name} FreeDict+WikDict dictionary</title>
+                     <respStmt>
+                        <resp>Maintainer</resp>
+                        <name>Karl Bartel</name>
+                     </respStmt>
                 </titleStmt>
-                <editionStmt><p>{today}</p></editionStmt>
+                <editionStmt><edition>{today}</edition></editionStmt>
                 <extent>{headwords} headwords</extent>
                 <publicationStmt>
                     <publisher>Karl Bartel</publisher>
@@ -59,15 +84,43 @@ with codecs.open(in_filename, 'r', 'utf-8') as in_file, \
                         <p>Licensed under the Creative Commons Attribution-ShareAlike License</p>
                         <p>See http://creativecommons.org/licenses/by-sa/3.0/ for details</p>
                     </availability>
+                    <date>{today}</date>
                 </publicationStmt>
                 <sourceDesc>
                     <p>All entries from Wiktionary.org via DBnary</p>
                 </sourceDesc>
             </fileDesc>
+            <encodingDesc>
+                <projectDesc>
+                    <p>
+                        This dictionary comes to you through nice people
+                        making it available for free and for good. It is part of
+                        the FreeDict project, http://www.freedict.org/. This
+                        project aims to make available many translating
+                        dictionaries for free. Your contributions are welcome!
+                    </p>
+                </projectDesc>
+                <tagsDecl>
+                    <!-- for each gi, its values are listed, with a pointer to the ontology interface -->
+                    <namespace name="http://www.tei-c.org/ns/1.0" xml:base="../shared/">
+                        <tagUsage gi="pos">
+                            <list n="values" type="bulleted">
+                                {pos_usage}
+                            </list>
+                        </tagUsage>
+                        <tagUsage gi="gen">
+                            <list>
+                                <item ana="FreeDict_ontology.xml#f_gen_fem">fem</item>
+                                <item ana="FreeDict_ontology.xml#f_gen_masc">masc</item>
+                            </list>
+                        </tagUsage>
+                    </namespace>
+                </tagsDecl>
+            </encodingDesc>
         </teiHeader>
     '''.format(
             from_name=language_names[from_lang], to_name=language_names[to_lang],
-            headwords=headwords, today=datetime.date.today()
+            headwords=headwords, today=datetime.date.today(), pos_usage=pos_usage
         ).encode('utf-8'))
     root.append(tei_header)
 
@@ -84,25 +137,37 @@ with codecs.open(in_filename, 'r', 'utf-8') as in_file, \
         entry = SubElement(body, 'entry')
         form = SubElement(entry, 'form')
         orth = SubElement(form, 'orth')
-        if x.pos == 'suffix' or (x.pos == '' and x.word.startswith('-')):
+        if x.pronun_list:
+            for p in x.pronun_list.split(' | '):
+                pron = SubElement(form, 'pron')
+                pron.text = p
+        is_suffix = x.pos == 'suffix' or (x.pos == '' and x.word.startswith('-'))
+        if is_suffix:
             assert x.word.startswith('-')
             orth.text = x.word[1:]
+            pos_text = 'suffix'
+        else:
+            orth.text = x.word
+            pos_text = pos_mapping.get(x.pos, (x.pos, None))[0]
+
+        # gramGrp
+        if pos_text:
             gram_grp = SubElement(entry, 'gramGrp')
             pos = SubElement(gram_grp, 'pos')
-            pos.text = 'part'
-        else:
-            orth.text = x.word + x.pos
+            pos.text = pos_text
 
         # translation
         cit = SubElement(entry, 'cit',
                          {'type': 'translation', 'xml:lang': to_lang})
         for trans in x.trans_list.split(' | '):
             quote = SubElement(cit, 'quote')
+            if is_suffix:
+                trans = trans[1:]
             quote.text = trans
 
         # sense
         sense = SubElement(entry, 'sense')
-        sense_def = SubElement(sense, 'def')
+        sense_def = SubElement(sense, 'usg')
         sense_def.text = x.sense
 
     # format xml output
