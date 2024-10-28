3b51ec6ee69b37946b9dac92ec05313bc4a8e834
Karl Bartel <karl@karl.berlin>
2024-10-28 21:11:36 +0100

    Remove unused attach_dbs function

--- src/run.py
+++ src/run.py
@@ -28,25 +28,6 @@ def search_query(from_lang, to_lang, search_term, **kwargs):
         print("%-40s %-20s %-80s %s" % r)
 
 
-def attach_dbs(from_lang, to_lang):
-    main_db_filename = "dictionaries/wdweb/wikdict.sqlite3"
-    if not os.path.isfile(main_db_filename):
-        conn = sqlite3.connect(main_db_filename)
-        with open("main.sql") as f:
-            conn.executescript(f.read())
-    return """
-        ATTACH DATABASE 'dictionaries/sqlite/{to_lang}.sqlite3' AS other;
-        ATTACH DATABASE 'dictionaries/sqlite/{from_lang}-{to_lang}.sqlite3' AS lang_pair;
-        ATTACH DATABASE 'dictionaries/sqlite/{to_lang}-{from_lang}.sqlite3' AS other_pair;
-        ATTACH DATABASE 'dictionaries/sqlite/prod/{from_lang}-{to_lang}.sqlite3' AS prod;
-        ATTACH DATABASE 'dictionaries/sqlite/prod/wikdict.sqlite3' AS wikdict;
-        ATTACH DATABASE 'dictionaries/sqlite/prod/{from_lang}.sqlite3' AS prod_lang;
-        ATTACH DATABASE 'dictionaries/sqlite/prod/{to_lang}.sqlite3' AS prod_other;
-    """.format(
-        from_lang=from_lang, to_lang=to_lang
-    )
-
-
 if __name__ == "__main__":
     sqlite3.enable_callback_tracebacks(True)
 
